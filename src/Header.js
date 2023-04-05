2149d711d795868526395f3d41b50f288864a36f
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-05 13:25:52 -0700

    applied basic styling to header

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,18 @@ function Header() {
 
 
     return(
-        <div>
+        <div className="headerContainer">
             <header className="header">
                 <h1 className="title1">Wizard Zane</h1>
                 <h2 className="subTitle1">for Events and Adventures</h2>
             </header>
+            <div className="navbar">
+                <ul className="navmenu">
+                    <li className="navItem">About Me</li>
+                    <li className="navItem">Wizard Skills</li>
+                    <li className="navItem">Bookings</li>
+                </ul>
+            </div>
         </div>
     )
 }
