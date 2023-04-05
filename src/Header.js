16eae824767f7e34419c958321ad47f94c743acd
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-05 12:56:20 -0700

    got basic file structure built

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+function Header() {
+
+
+    return(
+        <div>
+            <header>
+                <h1>Wizard Zane</h1>
+                <h2>for Events and Adventures</h2>
+            </header>
+        </div>
+    )
+}
+export default Header;
