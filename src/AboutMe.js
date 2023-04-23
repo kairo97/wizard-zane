b4cf83ce4be31afc5ec827f906a235572233a52c
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-04-23 13:42:37 -0700

    applied grid to about me and image

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,8 +4,10 @@ function AboutMe(){
     return(
         <div className="aboutMeContainer">
             <img className="wizImage" src={WizImage}/>
+            <div className="wizText">
             <h3>About Me</h3>
             <p> I am a pacific northwest local wizard known as Zane, You can find me out and about whether with my staff or pipe, I am always shrowded in an eire of mysticism. I am a rather young wizard being only 26 eons old. But what I lack in beard I make up for in magick. My services are available for hire, pricings vary based on your needs, but I am open to discussion and am sure we can find what works for you</p>
+            </div>
         </div>
     )
 }
